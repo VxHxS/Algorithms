142859f014cfac00a8001e2b71a797796708948b
Yuriy <103923663+VxHxS@users.noreply.github.com>
2024-05-29 12:21:47 +0300

    Binary search

---
+++ algo.js
@@ -0,0 +1,18 @@
+// Бинарный поиск
+const binarySearch = (list, item) => {
+    let low = 0;
+    let high = list.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high)  / 2);
+        let guess  = list[mid]
+        if (guess == item) {
+            return mid;
+        } else if ( guess > item) {
+            high = mid - 1;
+        } else  if ( guess < item) {
+            low = mid + 1;
+        } 
+    }
+    return undefined;
+}
